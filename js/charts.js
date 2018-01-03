7beb91cca1a293848da062712423b11bd95a139c
harryb0905 <harryb0905@googlemail.com>
2018-01-03 21:46:09 +0000

    Changed some color schemes and added working horizontal bar chart

--- js/charts.js
+++ js/charts.js
@@ -1,36 +1,53 @@
-window.onload = function() {
-     document.getElementById('portfolio-item-click').addEventListener('click', function() {
-         var ctx = document.getElementById("myChart").getContext('2d');
-         window.myChart = new Chart(ctx, {
-             type: 'doughnut',
-             data: {
-                 labels: ["HTML", "CSS", "JavaScript", "PHP"],
-                 datasets: [{
-                     label: 'Programming Languages',
-                     data: [39.4, 23.2, 36.5, 0.9],
-                     backgroundColor: [
-                         'rgba(255, 99, 132, 0.4)',
-                         'rgba(54, 162, 235, 0.4)',
-                         'rgba(255, 206, 86, 0.4)',
-                         'rgba(75, 192, 192, 0.4)',
-                     ],
-                     borderColor: [
-                         'rgba(255,99,132,1)',
-                         'rgba(54, 162, 235, 1)',
-                         'rgba(255, 206, 86, 1)',
-                         'rgba(75, 192, 192, 1)',
-                     ],
-                     borderWidth: 1
-                 }]
-             },
-             options: {
-                  animationSteps: 100,
-                  animationEasing: "easeOutBounce",
-                  animateRotate: true,
-                  animateScale: false,
-                  responsive: true
-             }
-         });
+ $(document).ready(function() {
+      var horizontalBarChartData = {
+          labels: ["HTML", "CSS", "JavaScript", "PHP"],
+          datasets: [{
+              backgroundColor: "#00b0f0",
+              data: [40,30,20,10]
+          }]
+      };
+      var ctx = document.getElementById("barChart").getContext("2d");
+      var myHorizontalBar = new Chart(ctx, {
+          type: 'horizontalBar',
+          data: horizontalBarChartData,
+          options: {
 
-     });
- };
+
+
+              scales: {
+
+                yAxes:[{
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Language',
+                    },
+                    barThickness: 20,
+                    ticks: {
+                        beginAtZero:true,
+                    },
+                }],
+                xAxes:[{
+
+                    scaleLabel: {
+                        display: true,
+                        labelString: '% of Project',
+                    },
+
+                    ticks: {
+                        beginAtZero:true
+                    },
+                }],
+              },
+              responsive: true,
+              legend: {
+                  display: false,
+              },
+              title: {
+                  display: true,
+                  text: 'Programming Languages Used (%)'
+              },
+
+              responsiveAnimationDuration: 1500
+          }
+      });
+});
